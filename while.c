1002ae47ac39d04ab57d700eb31c07ddbb103005
lzw-star <1241575823@qq.com>
2022-07-27 07:55:31 +0800

    first commit

--- while.c
+++ while.c
@@ -1,15 +1,1 @@
 #include<stdio.h>
-
-
-int main()
-{
-	printf("this is one question\n");
-	printf("this is two question\n");
-	printf("this is three question\n");
-	printf("this is four question\n");
-	printf("this is five question\n");
-	printf("this is six question\n");
-	while(1)
-		sleep(1);
-	return 0;
-}
