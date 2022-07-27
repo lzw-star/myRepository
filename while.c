5d62d4ff329b5286f66d60f80b0dac5fcf902d4b
lzw-star <1241575823@qq.com>
2022-07-27 08:01:41 +0800

    two commit

--- while.c
+++ while.c
@@ -1,1 +1,14 @@
 #include<stdio.h>
+
+int main()
+{
+	printf("this is one question\n");
+	printf("this is one question\n");
+	printf("this is one question\n");
+	printf("this is one question\n");
+	printf("this is one question\n");
+	printf("this is one question\n");
+	while(1)
+		sleep(1);
+	return 0;
+}
